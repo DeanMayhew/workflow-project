9d9efa60047a863e22bee97c40d08f1af62a74fc
Dean Mayhew <dean.mayhew@sinnerschrader.com>
2019-03-28 14:15:03 +0100

    feat: adjust task class to have getter and setter

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,74 @@
-// restructure data with flags
-// getter and setter for class
-// read class article again
-// adjust controller
-
-var data = {
-    open: [
-        {uniqueId: 1, title: 'render left side of ToDo list', description: 'with js, function? array with content of list'},
-        {uniqueId: 2, title: 'import node second parameter, meaning', description: ''}
-    ],
-    done: [
-        {uniqueId: 3, title: 'setup intial markup', description: ''},
-        {uniqueId: 4, title: 'learned about semantic', description: ''},
-        {uniqueId: 5, title: 'research for including resscources', description: '(Css, Js, etc., Html 5 boilerplate)'},
-        {uniqueId: 6, title: 'research reset/normalize browser styled', description: 'differance between reset and normalize! Current solutions(medium, mdn, google developers, etc.)'},
-        {uniqueId: 7, title: 'css custom properties, css-variablen', description: ''},
-        {uniqueId: 8, title: 'Js: dom api, selectoren, node children, parents, etc.', description: ''},
-        {uniqueId: 9, title: 'template tag', description: 'natives hmtl tag'}
-    ]
-};
-
-// var dataWithFlags = [
-//     {uniqueId: 1,title: '1 Task', description: '1 Description', isDone: false},
-//     {uniqueId: 2, title: '2 Task', description: '2 Description', isDone: true},
-//     {uniqueId: 3, title: '3 Task', description: '3 Description', isDone: false}
-// ]
+var dataWithFlags = [
+    {uniqueId: 1, title: 'setup intial markup', description: '', isDone: true},
+    {uniqueId: 2, title: 'learned about semantic', description: '', isDone: true},
+    {uniqueId: 3, title: 'research for including resscources', description: '(Css, Js, etc., Html 5 boilerplate)', isDone: true},
+    {uniqueId: 4, title: 'research reset/normalize browser styled', description: 'differance between reset and normalize! Current solutions(medium, mdn, google developers, etc.)', isDone: true},
+    {uniqueId: 5, title: 'css custom properties, css-variablen', description: '', isDone: true},
+    {uniqueId: 6, title: 'Js: dom api, selectoren, node children, parents, etc.', description: '', isDone: true},
+    {uniqueId: 7, title: 'template tag', description: 'natives hmtl tag', isDone: true},
+    {uniqueId: 8, title: 'render left side of ToDo list', description: 'with js, function? array with content of list', isDone: true},
+    {uniqueId: 9, title: 'import node second parameter, meaning', description: '', isDone: true},
+    {uniqueId: 10, title: 'restructure data with flags', description: '', isDone: true},
+    {uniqueId: 11, title: 'create getter and setter for Task class', description: '', isDone: true},
+    {uniqueId: 12, title: 'read the class article again', description: '', isDone: true},
+    {uniqueId: 13, title: 'adjust the controller to match the new data', description: '', isDone: true},
+]
 
 var templateSelector = document.querySelector('template');
 var todoListSelector = document.querySelector('ol');
 var doneListSelector = document.querySelector('ul');
 
-data.open.forEach((task) => {
+dataWithFlags.forEach(task => {
     var singleTaskElement = document.importNode(templateSelector.content, true).firstElementChild;
     singleTaskElement.querySelector('.list-item__title').innerText = task.title;
     Boolean(task.description) && (singleTaskElement.querySelector('.list-item__description').innerText = task.description);
-    
-    var test = todoListSelector.appendChild(singleTaskElement);
-    console.log(test);
-});
 
-data.done.forEach((task) => {
-    var singleTaskElement = document.importNode(templateSelector.content, true).firstElementChild;
-    singleTaskElement.querySelector('.list-item__title').innerText = task.title;
-    Boolean(task.description) && (singleTaskElement.querySelector('.list-item__description').innerText = task.description);
-    
-    doneListSelector.appendChild(singleTaskElement);
-});
+    if (task.isDone) {
+        doneListSelector.appendChild(singleTaskElement);
+    } else { 
+        todoListSelector.appendChild(singleTaskElement);
+    }
+})
 
 class Task {
-
     constructor(data) {
         this._title = data.title;
         this._description = data.description;
         this._uniqueId = data.uniqueId;
+        this._isDone = data.isDone;
+    }
+
+    set title(title) {
+        this._title = title;
+    }
+    
+    get title() {
+        return this._title;
+    }
+
+    set description(description) {
+        this._description = description;
+    }
+    
+    get description() {
+        return this._description;
+    }
+
+    set uniqueId(uniqueId) {
+        this._uniqueId = uniqueId;
+    }
+    
+    get uniqueId() {
+        return this._uniqueId;
+    }
+
+    set isDone(isDone) {
+        this._isDone = isDone;
+    }
+    
+    get isDone() {
+        return this._isDone;
     }
 }
 
-console.log(new Task(data.open[0]));
+console.log(new Task(dataWithFlags[0]));
